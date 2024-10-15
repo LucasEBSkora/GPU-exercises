77a6a9886c826ae58e2e0bfe3b801ecab105d467
LucasEBSkora <skora.lucas@gmail.com>
2024-10-15 08:20:43 +0200

    commit for first class

---
+++ base.cl
@@ -0,0 +1,8 @@
+__kernel void summation(
+    __global const int* a,
+    __global const int* b,
+    __global       int* c 
+    ) {
+    int i = get_global_id(0);
+    c[i] = a[i] + b[i];
+}
--- base.cpp
+++ base.cpp
@@ -2,7 +2,6 @@
 
 // ----------------------------------------------------------
 
-
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
@@ -11,7 +10,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <iomanip> 
+#include <iomanip>
 #include <chrono>
 
 using namespace std;
@@ -25,7 +24,7 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	const char *clu_File = SRC_PATH "base.cl";  // path to file containing OpenCL kernel(s) code
+	const char *clu_File = SRC_PATH "prefetch.cl"; // path to file containing OpenCL kernel(s) code
 
 	// Initialize OpenCL
 	cluInit();
@@ -37,4 +36,41 @@ int main(int argc, char **argv)
 
 	// Load Program
 	cl::Program *program = cluLoadProgram(clu_File);
+
+	cl::Kernel *kernel = cluLoadKernel(program, "summation");
+
+	const int size = 32;
+	const int group_size = 8;
+	cl::Buffer a_buffer(*clu_Context, CL_MEM_READ_ONLY, size * sizeof(int));
+	cl::Buffer b_buffer(*clu_Context, CL_MEM_READ_ONLY, size * sizeof(int));
+	cl::Buffer c_buffer(*clu_Context, CL_MEM_WRITE_ONLY, size * sizeof(int));
+
+	int *a = new int[size];
+	int *b = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		a[i] = 32 - i;
+		b[i] = 2 * i + 1;
+	}
+
+	clu_Queue->enqueueWriteBuffer(a_buffer, true, 0, size * sizeof(int), a);
+	clu_Queue->enqueueWriteBuffer(b_buffer, true, 0, size * sizeof(int), b);
+	delete[] a;
+	delete[] b;
+
+	kernel->setArg(0, a_buffer);
+	kernel->setArg(1, b_buffer);
+	kernel->setArg(2, c_buffer);
+	kernel->setArg(3, cl::__local(group_size*sizeof(int)));
+	
+	clu_Queue->enqueueNDRangeKernel(*kernel, cl::NullRange,
+									cl::NDRange(size),  // Global work size: number of kernels
+									cl::NDRange(group_size)); // Local work size
+	clu_Queue->finish();
+
+	int *c = new int[size];
+	clu_Queue->enqueueReadBuffer(c_buffer, true, 0, size * sizeof(int), c);
+	for (int i = 0; i < size; i++) {
+		printf("c[%d] = %d\n", i, c[i]);
+	}
 }
